990bdec562b32c58c23fafc964889e92996f8024
saurabh sharma <saurabh.sharma@perennialsys.com>
2024-02-03 17:21:26 +0530

    Base url in cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,6 +4,20 @@
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_myweatherapp_resource_Keys_apiKey(JNIEnv *env, jobject thiz) {
-    std::string api_key = "place your key here";
+    std::string api_key = "Place your api key";
+    return env->NewStringUTF(api_key.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_myweatherapp_resource_Constants_getBaseUrl(JNIEnv *env, jobject thiz) {
+    std::string api_key = "https://api.openweathermap.org/data/2.5/";
+    return env->NewStringUTF(api_key.c_str());
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_myweatherapp_resource_Constants_getImageUrl(JNIEnv *env, jobject thiz) {
+    std::string api_key = "https://openweathermap.org/img/wn/";
     return env->NewStringUTF(api_key.c_str());
 }
