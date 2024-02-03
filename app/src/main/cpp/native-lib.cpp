59ca570d8daa2fcfb8efab504fc9cd681c0bf69c
saurabh sharma <saurabh.sharma@perennialsys.com>
2024-02-03 09:08:30 +0530

    removed api key

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,6 +4,6 @@
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_myweatherapp_resource_Keys_apiKey(JNIEnv *env, jobject thiz) {
-    std::string api_key = "MTE4MDQ2ZWQyM2QzOTE3ZmI4NjYyOGMwZTQ3YzdhODU=";
+    std::string api_key = "place your key here";
     return env->NewStringUTF(api_key.c_str());
 }
